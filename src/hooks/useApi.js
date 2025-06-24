381c3d0fc0819fc42d4134ecfb607fec43a47ff3
GianQuse <gianquse@gmail.com>
2025-06-24 18:04:35 -0300

    ajuste scroll

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -47,7 +47,7 @@ export function useApiDetail(ID) {
     const { items, setItems, loading, setLoading } = useApiState();
 
     useEffect(() => {
-        const db = getFirestore()
+        const db = getFirestore();
         const menuCollection = collection(db, 'menu');
         getDocs(menuCollection).then((response) => {
             const responseMapped = response.docs.map((doc) => ({ ...doc.data() }));
@@ -65,10 +65,19 @@ export function useApiDetail(ID) {
             setItems(platoEncontrado);
         }).finally(() => {
             setLoading(false);
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
+
+            const scrollOptions = { top: 0, behavior: 'smooth' };
+
+            // Para compatibilidad con todos los navegadores
+            if (document.documentElement) {
+                document.documentElement.scrollTo(scrollOptions);
+            }
+
+            if (document.body) {
+                document.body.scrollTo(scrollOptions);
+            }
+
+            window.scrollTo(scrollOptions);
         });
     }, []);
 
